fc47ca5e4f5d700b3a94913fac9c8149fe89181d
little8hwq <maggiehwq@gmail.com>
2015-01-08 22:16:51 +0800

    Modify storyboard

---
+++ Drawear/Drawear/controller/AccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountViewController.h
+//  Drawear
+//
+//  Created by 何文琦 on 1/8/15.
+//  Copyright (c) 2015 DrawearGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountViewController : UIViewController
+
+@end
